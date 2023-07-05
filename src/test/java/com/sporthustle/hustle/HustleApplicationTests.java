ae1cf9f0afec2884439eb0bb68cda581197683b9
kor-Chipmunk <rhj4862@gmail.com>
2023-07-05 20:59:33 +0900

    [Feature] - #1 기본 프로젝트 생성

---
+++ src/test/java/com/sporthustle/hustle/HustleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sporthustle.hustle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HustleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
