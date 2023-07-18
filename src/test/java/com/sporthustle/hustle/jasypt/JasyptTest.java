5d3bf95986357349d5fa3976be4808cd6a9b0f7c
kor-Chipmunk <rhj4862@gmail.com>
2023-07-18 17:39:46 +0900

    test: 평문 암호화하여 출력해주는 `jasypt` 테스트 코드 작성

---
+++ src/test/java/com/sporthustle/hustle/jasypt/JasyptTest.java
@@ -0,0 +1,28 @@
+package com.sporthustle.hustle.jasypt;
+
+import com.sporthustle.hustle.config.JasyptConfig;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JasyptTest extends JasyptConfig {
+
+    @Test
+    public void 평문_암호화_결과_출력() {
+        String encryptKey = System.getProperty("jasypt.encryptor.password");
+
+        // 암호화할 평문 작성하기
+        String plainText = "";
+
+        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+        jasypt.setPassword(encryptKey);
+
+        String encryptedText = jasypt.encrypt(plainText);
+        String decryptedText = jasypt.decrypt(encryptedText);
+
+        System.out.println(encryptedText);
+
+        assertThat(plainText).isEqualTo(decryptedText);
+    }
+}
