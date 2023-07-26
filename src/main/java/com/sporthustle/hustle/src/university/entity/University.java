e42444c0b43f163ab464493978a230706a5f276d
Min Gyu <kimmingyu9811@gmail.com>
2023-07-27 00:47:13 +0900

    fix: user와 단방향 연관관계를 위한 University엔티티 수정

--- src/main/java/com/sporthustle/hustle/src/university/entity/University.java
+++ src/main/java/com/sporthustle/hustle/src/university/entity/University.java
@@ -1,7 +1,6 @@
 package com.sporthustle.hustle.src.university.entity;
 
 import com.sporthustle.hustle.common.entity.BaseEntity;
-import com.sporthustle.hustle.src.user.entity.User;
 import javax.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -23,7 +22,4 @@ public class University extends BaseEntity {
 
   @Column(name = "addess")
   private String address;
-
-  @OneToOne(mappedBy = "university")
-  private User user;
 }
