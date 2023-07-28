e795226437ade4f499f0484206b77ce4dcc127f8
Min Gyu <kimmingyu9811@gmail.com>
2023-07-28 17:03:44 +0900

    fix: constants 필요없는 요소 삭제

--- src/main/java/com/sporthustle/hustle/common/consts/Constants.java
+++ src/main/java/com/sporthustle/hustle/common/consts/Constants.java
@@ -6,7 +6,4 @@ public class Constants {
   public static final String REFRESH_TOKEN = "REFRESH_TOKEN";
   public static final String TOKEN_TYPE = "type";
   public static final Long TOKEN_VALID_TIME = 30 * 60 * 1000L;
-  public static final int TEMPORARY_PASSWORD_LENGTH = 10;
-  public static final int RANDOM_NUM_ORIGIN = 97;
-  public static final int RANDOM_NUM_BOUND = 122;
 }
