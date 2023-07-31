4f041805dad215f07ee9cc421f00e9a791f95793
Min Gyu <kimmingyu9811@gmail.com>
2023-07-31 20:54:56 +0900

    fix: dto에서 불필요한 필드 삭제

--- src/main/java/com/sporthustle/hustle/src/user/model/ResetPasswordReq.java
+++ src/main/java/com/sporthustle/hustle/src/user/model/ResetPasswordReq.java
@@ -1,6 +1,5 @@
 package com.sporthustle.hustle.src.user.model;
 
-import java.util.Date;
 import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 
@@ -9,12 +8,6 @@ public class ResetPasswordReq {
 
   @NotBlank(message = "아이디값은 필수 입력 값입니다.")
   private String email;
-
-  @NotBlank(message = "이름은 필수 입력 값입니다.")
-  private String name;
-
-  @NotBlank(message = "생년월일은 필수 입력 값입니다.")
-  private Date birth;
 
   @NotBlank(message = "새 비밀번호는 필수 입력 값입니다.")
   private String newPassword;
