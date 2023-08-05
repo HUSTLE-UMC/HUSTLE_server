f2546229a72fbeecb05cc4f1a6e1c0e6e15c872e
thdgustjd1 <thdgustjd1@naver.com>
2023-08-05 14:35:30 +0900

    feat: 대회참여팀 엔티티

---
+++ src/main/java/com/sporthustle/hustle/src/competition/entity/EntryTeam.java
@@ -0,0 +1,55 @@
+package com.sporthustle.hustle.src.competition.entity;
+
+
+import com.sporthustle.hustle.src.competition.Repository.CompetitionRepository;
+import com.sporthustle.hustle.src.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@Entity
+@NoArgsConstructor
+@Table(name = "EntryTeam")
+public class EntryTeam {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "entryTeam_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "competition_id")
+    private Competition competition;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "club_id")
+    //private Club club;  클럽 추후 구현
+
+
+    @Column(name = "name", length = 20)
+    private String name;
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+    @Column(name = "score")
+    private int score;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "update_at")
+    private Timestamp updateAt;
+
+}
