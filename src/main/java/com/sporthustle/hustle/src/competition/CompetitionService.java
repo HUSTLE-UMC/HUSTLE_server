ee0579fcfca08747ca1f17f31f1f99951449e953
thdgustjd1 <thdgustjd1@naver.com>
2023-08-07 17:06:39 +0900

    fix : CompetitionService 수정
    1. createCompetition의 연락처 받는 로직 수정
    2. Timestamp->LocalDateTime의 교체로 인한 기간설정 로직 교체
    3. readCompetition : 종목명의 입력여부에 따라 모든종목조회인지, 특정종목조회인지 구분 및 해당 로직들 구현

--- src/main/java/com/sporthustle/hustle/src/competition/CompetitionService.java
+++ src/main/java/com/sporthustle/hustle/src/competition/CompetitionService.java
@@ -3,7 +3,8 @@ package com.sporthustle.hustle.src.competition;
 import com.sporthustle.hustle.common.exception.BaseException;
 import com.sporthustle.hustle.common.exception.ErrorCode;
 import com.sporthustle.hustle.src.club.entity.Club;
-import com.sporthustle.hustle.src.club.model.ClubRepository;
+import com.sporthustle.hustle.src.club.Repository.ClubRepository;
+import com.sporthustle.hustle.src.club.model.ClubDTO;
 import com.sporthustle.hustle.src.competition.Repository.CompetitionContactRepository;
 import com.sporthustle.hustle.src.competition.Repository.CompetitionRepository;
 import com.sporthustle.hustle.src.competition.Repository.EntryTeamRepository;
@@ -11,7 +12,10 @@ import com.sporthustle.hustle.src.competition.entity.Competition;
 import com.sporthustle.hustle.src.competition.entity.CompetitionContact;
 import com.sporthustle.hustle.src.competition.entity.EntryTeam;
 import com.sporthustle.hustle.src.competition.model.*;
+import com.sporthustle.hustle.src.sportevent.entity.SportEvent;
+import com.sporthustle.hustle.src.sportposition.entity.SportEventRepository;
 import com.sporthustle.hustle.src.user.entity.User;
+import com.sporthustle.hustle.src.user.model.JoinRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
@@ -20,11 +24,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
-import java.sql.Timestamp;
-import java.time.YearMonth;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -35,15 +38,16 @@ public class CompetitionService {
     private final CompetitionRepository competitionRepository;
     private final CompetitionContactRepository competitionContactRepository;
     private final EntryTeamRepository entryTeamRepository;
-    private final ReadCompetitionInfoRes readCompetitionInfoRes;
     private final ClubRepository clubRepository;
+    private final SportEventRepository sportEventRepository;
 
 
     @Transactional
-    public HostRes createCompetition(HostReq hostreq){
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    public HostRes createCompetition(HostReq hostreq, String event){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) authentication.getPrincipal();
+
+        SportEvent sportEvent = sportEventRepository.findByName(event);
 
         Competition competition = Competition.builder()
                 .title(hostreq.getTitle())
@@ -59,96 +63,121 @@ public class CompetitionService {
                 .finalRoundTeamCount(hostreq.getFinalRoundTeamCount())
                 .sponsor(hostreq.getSponsor())
                 .place(hostreq.getPlace())
-                .status("모집중") //영어로
-                .createdAt(timestamp)
-                .updateAt(timestamp)
+                .state("DRAFT")
+                .type("BEFORE")
+                .status("")
                 .user(user)
+                .sportEvent(sportEvent)
                 .build();
 
         competitionRepository.save(competition);
 
-
-        CompetitionContact competitionContact1 = CompetitionContact.builder()
+        CompetitionContact mainCompetitionContact = CompetitionContact.builder()
                 .competition(competition)
                 .name(hostreq.getMainName())
                 .phoneNumber(hostreq.getMainPhoneNumber())
-                .position("main")
-                .createdAt(timestamp)
-                .updateAt(timestamp)
                 .build();
+        competitionContactRepository.save(mainCompetitionContact);
 
-        competitionContactRepository.save(competitionContact1);
+        if(hostreq.getSubName()!=null) {
+            CompetitionContact subCompetitionContact = CompetitionContact.builder()
+                    .competition(competition)
+                    .name(hostreq.getSubName())
+                    .phoneNumber(hostreq.getSubPhoneNumber())
+                    .build();
 
-        CompetitionContact competitionContact2 = CompetitionContact.builder()
-                .competition(competition)
-                .name(hostreq.getSubName())
-                .phoneNumber(hostreq.getSubPhoneNumber())
-                .position("sub")
-                .createdAt(timestamp)
-                .updateAt(timestamp)
-                .build();
-
-            competitionContactRepository.save(competitionContact2);
+            competitionContactRepository.save(subCompetitionContact);
+        }
 
         return HostRes.builder().message("대회 등록이 완료되었습니다.").build();
     }
-    @Transactional
-    public List<ReadCompetitionRes> readCompetitionByMonth(int month, int year, Pageable pageable){
-        YearMonth yearMonth = YearMonth.of(year, month);
-        int lastDay = yearMonth.lengthOfMonth();
 
-        Timestamp searchStart = new Timestamp(year, month, 1, 00,00,00,000);
-        Timestamp searchEnd = new Timestamp(year, month, lastDay,23,59,59,999);
-        List<Competition> competitions = competitionRepository.findAllByStartDateBetweenOrEndDateBetween(pageable,searchStart,searchEnd,searchStart,searchEnd).toList();
-        List<ReadCompetitionRes>result = new ArrayList<>();
-        for(Competition c : competitions){
-            ReadCompetitionRes readCompetitionRes = ReadCompetitionRes.builder()
-                    .posterUrl(c.getPosterUrl())
-                    .title(c.getTitle())
-                    .startDate(c.getStartDate())
-                    .endDate(c.getEndDate())
-                    .build();
-            result.add(readCompetitionRes);
+
+    @Transactional(readOnly = true)
+    public List<ReadCompetitionRes> readCompetition(int month, int year, Pageable pageable, String event){
+
+        LocalDateTime standard = LocalDateTime.of(year, month,1, 0,0,0);
+        LocalDateTime firstDate = standard.withDayOfMonth(1);
+        LocalDateTime lastDate = standard.withDayOfMonth(standard.toLocalDate().lengthOfMonth());
+        List<Competition> competitions =
+                //competitionRepository.findAllByStartDateBetweenOrEndDateBetween(pageable,firstDate,lastDate,firstDate,lastDate).toList();
+                competitionRepository.findByDateTime(firstDate, lastDate, pageable).toList();
+
+
+        //모든종목 조회
+        if(event == null || event.equals("")) {
+
+            List<ReadCompetitionRes> result = competitions.stream()
+                    .map(
+                            o ->
+                                    ReadCompetitionRes.builder()
+                                            .competitionId(o.getId())
+                                            .posterUrl(o.getPosterUrl())
+                                            .title(o.getTitle())
+                                            .startDate(o.getStartDate())
+                                            .endDate(o.getEndDate())
+                                            .build()
+                    ).collect(Collectors.toList());
+
+            return result;
+            }
+        //특정종목 조회
+        else
+        {
+            List<ReadCompetitionRes> result = new ArrayList<>();
+
+            for(Competition c : competitions){
+                if(c.getSportEvent().getName().equals(event)){
+                    ReadCompetitionRes readCompetitionRes = ReadCompetitionRes.builder()
+                                    .competitionId(c.getId())
+                                    .posterUrl(c.getPosterUrl())
+                                    .title(c.getTitle())
+                                    .startDate(c.getStartDate())
+                                    .endDate(c.getEndDate())
+                                    .build();
+
+                    result.add(readCompetitionRes);
+                }
+            }
+
+            return result;
+
         }
-        return result;
-    }
-    @Transactional
-    public List<ReadCompetitionRes> readCompetitionByEvent(int month,int year,Pageable pageable,String event){
-        YearMonth yearMonth = YearMonth.of(year, month);
-        int lastDay = yearMonth.lengthOfMonth();
-
-        Timestamp searchStart = new Timestamp(year, month, 1, 00,00,00,000);
-        Timestamp searchEnd = new Timestamp(year, month, lastDay,23,59,59,999);
-        List<Competition> competitions = competitionRepository.findAllByStartDateBetweenOrEndDateBetweenAndEvent(pageable,searchStart,searchEnd,searchStart,searchEnd,event).toList();
-        List<ReadCompetitionRes>result = new ArrayList<>();
-        for(Competition c : competitions){
-            ReadCompetitionRes readCompetitionRes = ReadCompetitionRes.builder()
-                    .posterUrl(c.getPosterUrl())
-                    .title(c.getTitle())
-                    .startDate(c.getStartDate())
-                    .endDate(c.getEndDate())
-                    .build();
-            result.add(readCompetitionRes);
-        }
-        return result;
     }
 
-    @Transactional
-    public ReadCompetitionInfoRes readCompetitionInfo(Long competitionId) {
+
+    @Transactional(readOnly = true)
+    public ReadCompetitionRes readCompetitionById(Long competitionId) {
+
         Competition competition = competitionRepository.findById(competitionId)
                 .orElseThrow(() -> new BaseException(ErrorCode.COMPETITION_NOT_FOUND));
 
         List<CompetitionContact> competitionContact = competitionContactRepository.findByCompetition(competition);
-        String mainPhoneNumber = "";
-        String subPhoneNumber = "";
-        for(CompetitionContact c : competitionContact){
-            if(c.getPosition().equals("main"))mainPhoneNumber = c.getPhoneNumber();
-            if(c.getPosition().equals("sub"))subPhoneNumber = c.getPhoneNumber();
-        }
 
-        List<Club> entryClubList = clubRepository.findByCompetitionId(competitionId);
 
-        ReadCompetitionInfoRes readCompetitionInfoRes = ReadCompetitionInfoRes.builder()
+        List<ContactDTO> contactDTOS = competitionContact.stream()
+                .map(
+                        o->
+                                ContactDTO.builder()
+                                        .Name(o.getName())
+                                        .phoneNumber(o.getPhoneNumber())
+                                        .build())
+                .collect(Collectors.toList());
+
+        List<EntryTeam> entryClubList = entryTeamRepository.findByCompetition(competition);
+
+        List<ClubDTO>clubDTOS = entryClubList.stream()
+                .map(
+                        o->
+                                ClubDTO.builder()
+                                        .profileImageUrl(o.getClub().getProfileImageUrl())
+                                        .name(o.getClub().getName())
+                                        .universityName(o.getClub().getUniversity().getName())
+                                        .build())
+                .collect(Collectors.toList());
+
+
+        ReadCompetitionRes readCompetitionRes = ReadCompetitionRes.builder()
                 .startDate(competition.getStartDate())
                 .endDate(competition.getEndDate())
                 .recruitmentStartDate(competition.getRecruitmentStartDate())
@@ -157,38 +186,40 @@ public class CompetitionService {
                 .maxEntryCount(competition.getMaxEntryCount())
                 .sponsor(competition.getSponsor())
                 .place(competition.getPlace())
-                .mainPhoneNumber(mainPhoneNumber)
-                .subPhoneNumber(subPhoneNumber)
+                .contactDTO(contactDTOS)
                 .numberOfTeam(entryClubList.size())
-                .clubList(entryClubList)
+                .clubDTOS(clubDTOS)
                 .build();
 
-        return readCompetitionInfoRes;
+        return readCompetitionRes;
     }
+
+
+
     @Transactional
     public EntryRes entryCompetition(EntryReq entryReq, Long competitionId){
-        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
         Competition competition = competitionRepository.findById(competitionId)
                         .orElseThrow(() -> new BaseException(ErrorCode.COMPETITION_NOT_FOUND));
 
+        if(entryTeamRepository.countByCompetition(competition) >= competition.getMaxEntryCount()){
+            return EntryRes.builder().message("이미 참가신청이 완료된 대회입니다.").build();
+        }
+
         Club club = clubRepository.findByName(entryReq.getClubName());
+
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User) authentication.getPrincipal();
 
         EntryTeam entryTeam = EntryTeam.builder()
                 .competition(competition)
-                .userId(user)
+                .user(user)
                 .club(club)
                 .name(entryReq.getMainName())
                 .phoneNumber(entryReq.getMainPhoneNumber())
-                .score(0)
-                .createdAt(timestamp)
-                .updateAt(timestamp)
+                .score(0L)
                 .build();
         entryTeamRepository.save(entryTeam);
-
-        //status를 모집중 -> 모집완료로 바꾸는 로직필요
 
         return EntryRes.builder().message("대회 신청이 완료되었습니다.").build();
     }
