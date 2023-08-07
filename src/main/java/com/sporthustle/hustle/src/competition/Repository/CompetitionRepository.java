6a876f642c030c2d899ad3770d58ef8e0fbe2de7
thdgustjd1 <thdgustjd1@naver.com>
2023-08-07 17:01:39 +0900

    fix : CompetitionRepository 수정
    1. 모든 대회조회를 커스텀 쿼리로 수정

--- src/main/java/com/sporthustle/hustle/src/competition/Repository/CompetitionRepository.java
+++ src/main/java/com/sporthustle/hustle/src/competition/Repository/CompetitionRepository.java
@@ -5,14 +5,21 @@ import com.sporthustle.hustle.src.competition.model.ReadCompetitionRes;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface CompetitionRepository extends JpaRepository<Competition, Long> {
-    Page<Competition> findAllByStartDateBetweenOrEndDateBetween(Pageable pageable, Timestamp startDateT1, Timestamp startDateT2, Timestamp endDateT1, Timestamp endDateT2);
+    //Page<Competition> findAllByStartDateBetweenOrEndDateBetween(Pageable pageable, LocalDateTime startDateT1, LocalDateTime endDateT1,LocalDateTime startDateT2, LocalDateTime endDateT2);
+    @Query(nativeQuery = true,
+            value = "select * from competition c" + " where c.start_date between :startDate and :endDate " +
+            "or c.end_date between :startDate and :endDate")
+    Page<Competition> findByDateTime(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, Pageable pageable);
 
-    Page<Competition> findAllByStartDateBetweenOrEndDateBetweenAndEvent(Pageable pageable, Timestamp startDateT1, Timestamp startDateT2, Timestamp endDateT1, Timestamp endDateT2, String event);
+    //Page<Competition> findAllByStartDateBetweenOrEndDateBetweenAndSportEvent_Name(Pageable pageable, LocalDateTime startDateT1, LocalDateTime startDateT2, LocalDateTime endDateT1, LocalDateTime endDateT2, String event);
 }
