67fe6e19985f353e191aaf567b81ba6b989cb222
thdgustjd1 <thdgustjd1@naver.com>
2023-08-07 17:00:18 +0900

    fix : CompetitionController 수정
    1.@RequestMapping 추가
    2.일부 파라미터를 @PathVariable -> @RequestParam으로 교체
    3.기존의 모든대회조회, 특정종목대회조회 메소드를 하나로 통일

--- src/main/java/com/sporthustle/hustle/src/competition/CompetitionController.java
+++ src/main/java/com/sporthustle/hustle/src/competition/CompetitionController.java
@@ -1,6 +1,7 @@
 package com.sporthustle.hustle.src.competition;
 
 import com.sporthustle.hustle.src.competition.model.*;
+import com.sporthustle.hustle.src.user.model.JoinRes;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -13,39 +14,42 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Tag(name = "Competition")
+@RequestMapping("/api/competition")
 @RequiredArgsConstructor
 @RestController
 
 public class CompetitionController {
     private final CompetitionService competitionService;
 
-    @PostMapping("/competition") //대회등록 (주최자)
-    public ResponseEntity<HostRes> createCompetition(@RequestBody HostReq hostReq){ //신청자 id 어떻게?
-        HostRes hostRes = competitionService.createCompetition(hostReq);
+    @PostMapping //대회등록 (주최자)
+    public ResponseEntity<HostRes> createCompetition(@RequestBody HostReq hostReq, @RequestParam("event") String event){
+        HostRes hostRes = competitionService.createCompetition(hostReq, event);
         return ResponseEntity.ok(hostRes);
     }
-    @GetMapping("/competition/{year}/{month}") //월별대회조회(종목상관없이)
-    public ResponseEntity<List<ReadCompetitionRes>> readCompetitionByMonth(@PathVariable("month")int month, @PathVariable("year")int year, @PageableDefault(size = 4, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
-        List<ReadCompetitionRes> data = competitionService.readCompetitionByMonth(month, year, pageable);
-        return ResponseEntity.ok(data);
-    }
 
-    @GetMapping("/competition/{year}/{month}/{event}") // 종목별 대회조회 (월별)
-    public ResponseEntity<List<ReadCompetitionRes>> readCompetitionByEvent(@PathVariable("month")int month, @PathVariable("year")int year, @PageableDefault(size = 12, sort = "id", direction = Sort.Direction.DESC) Pageable pageable, @PathVariable("event")String event){
-        List<ReadCompetitionRes> data = competitionService.readCompetitionByEvent(month,year,pageable,event);
-        return ResponseEntity.ok(data);
-    }
 
-    @GetMapping("/competition/{competitionId}") // 해당 대회에 참가하고있는 팀, 참가현황,  대회상세정보 조회
-    public ResponseEntity<ReadCompetitionInfoRes> readCompetitionInfo(@PathVariable("competitionId") Long competitionId){
-        ReadCompetitionInfoRes data = competitionService.readCompetitionInfo(competitionId);
+    @GetMapping //대회조회(event==null일 경우 year,month로만 조회, event!=null일 경우 특정종목만 조회)
+    public ResponseEntity<List<ReadCompetitionRes>> readCompetitionByMonth(@RequestParam(value = "month",required = false)int month
+            , @RequestParam(value = "year", required = false)int year
+            , @RequestParam(value = "event", required = false) String event
+            , @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        List<ReadCompetitionRes> data = competitionService.readCompetition(month, year, pageable, event);
         return ResponseEntity.ok(data);
     }
 
 
 
-    @PostMapping("/competition/{competitionId}") //대회신청 (참여자)
-    public ResponseEntity<EntryRes> entryCompetition(@RequestBody EntryReq entryReq, @PathVariable("competitionId") Long competitionId){
+    @GetMapping("/{competitionId}") // 해당 대회에 참가하고있는 팀, 참가현황,  대회상세정보 조회
+    public ResponseEntity<ReadCompetitionRes> readCompetitionById(@PathVariable("competitionId") Long competitionId){
+        ReadCompetitionRes data = competitionService.readCompetitionById(competitionId);
+        return ResponseEntity.ok(data);
+    }
+
+
+
+    @PostMapping("/{competitionId}") //대회신청 (참여자)
+    public ResponseEntity<EntryRes> entryCompetition(@RequestBody EntryReq entryReq
+            , @PathVariable("competitionId") Long competitionId){
         EntryRes entityRes = competitionService.entryCompetition(entryReq, competitionId);
         return ResponseEntity.ok(entityRes);
     }
