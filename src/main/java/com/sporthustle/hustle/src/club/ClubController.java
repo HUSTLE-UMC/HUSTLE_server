b4abb291f01211068556415f4dd774876103927a
realisshomyang <realisshoman@kakao.com>
2023-08-09 16:27:26 +0900

    [Fix/#10] Club등록 , 특정 동아리 id로 조회 구현

--- src/main/java/com/sporthustle/hustle/src/club/ClubController.java
+++ src/main/java/com/sporthustle/hustle/src/club/ClubController.java
@@ -1,7 +1,12 @@
 package com.sporthustle.hustle.src.club;
 
+import com.sporthustle.hustle.src.club.dto.ClubDto;
+import com.sporthustle.hustle.src.club.dto.InputReq;
+import com.sporthustle.hustle.src.club.dto.InputRes;
 import com.sporthustle.hustle.src.club.entity.Club;
 import com.sporthustle.hustle.src.club.ClubRepository;
+import com.sporthustle.hustle.src.user.model.JoinReq;
+import com.sporthustle.hustle.src.user.model.JoinRes;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +21,17 @@ import java.util.List;
 public class ClubController {
     private final ClubService clubService;
     @PostMapping
-    public ResponseEntity<Club> registerClub(@RequestBody Club club) {
-        Club savedClub = clubService.registerClub(club);
+    public ResponseEntity<InputRes> registerClub(@RequestBody InputReq inputReq) {
+        InputRes savedClub = clubService.registerClub(inputReq);
         return new ResponseEntity<>(savedClub, HttpStatus.CREATED);
     }
-
     @GetMapping("/{id}")
-    public ResponseEntity<Club> getClubById(@PathVariable Long id) {
-        Club club = clubService.getClubById(id);
-        if (club == null) {
+    public ResponseEntity<ClubDto> getClubById(@PathVariable Long id) {
+        ClubDto clubDto = clubService.getClubById(id);
+        if (clubDto == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(club, HttpStatus.OK);
+        return new ResponseEntity<>(clubDto, HttpStatus.OK);
     }
     @GetMapping
     public ResponseEntity<List<Club>> getAllClubs() {
