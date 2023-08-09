4c4a55b99dd16d37c7950c65854f77c92848c902
realisshomyang <realisshoman@kakao.com>
2023-08-09 16:24:36 +0900

    [Feat/#10] 에러코드에 universitynotfound , sporteventnotfound 추가

--- src/main/java/com/sporthustle/hustle/common/exception/ErrorCode.java
+++ src/main/java/com/sporthustle/hustle/common/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
   ALREADY_EXIST_USER(BAD_REQUEST, "ALREADY_EXIST_USER", "이미 존재하는 유저입니다."),
   USER_NOT_FOUND(NOT_FOUND, "USER_NOT_FOUND", "해당 유저를 찾을 수 없습니다."),
   INVALID_PASSWORD(BAD_REQUEST, "INVALID_PASSWORD", "잘못된 비밀번호 입니다."),
-  ;
+    UNIVERSITY_NOT_FOUND(NOT_FOUND,"UNIVERSITY_NOT_FOUND", "해당 대학교 정보를 찾을 수 없습니다."),
+  SPORT_EVENT_NOT_FOUND(NOT_FOUND,"SPORT_EVENT_NOT_FOUND" ,"해당 sportevent를 찾을 수 없습니다." );
 
   private final HttpStatus status;
   private final String code;
