4e72c8d276173f325f00c6ae9b85f63d0dba6316
kor-Chipmunk <rhj4862@gmail.com>
2023-08-13 01:13:48 +0900

    feat: `SportEvent` 엔티티 & `SportPosition` 엔티티 작성

---
+++ src/main/java/com/sporthustle/hustle/sport/entity/SportEvent.java
@@ -0,0 +1,26 @@
+package com.sporthustle.hustle.sport.entity;
+
+import com.sporthustle.hustle.common.entity.BaseEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "SportEvent")
+public class SportEvent extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @OneToMany(mappedBy = "sportEvent")
+  private List<SportPosition> sportPositions = new ArrayList<>();
+}
