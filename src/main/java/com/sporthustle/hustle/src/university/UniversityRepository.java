ddbe0524bfcebaf8aae00a5c143e4d6453762bdd
kor-Chipmunk <rhj4862@gmail.com>
2023-08-13 04:07:17 +0900

    chore: 충돌난 `src` 폴더 삭제

--- src/main/java/com/sporthustle/hustle/src/university/UniversityRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sporthustle.hustle.src.university;
-
-import com.sporthustle.hustle.src.university.entity.University;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UniversityRepository extends JpaRepository<University, Long> {}
