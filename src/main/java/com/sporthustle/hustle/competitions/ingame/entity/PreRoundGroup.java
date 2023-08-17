91df3de3268921c67fc04420a64902d646b51491
kor-Chipmunk <rhj4862@gmail.com>
2023-08-17 12:05:46 +0900

    feat: `PreRoundGroup` 생성자 빌더 및 대회 & 참가팀 연관관게 업데이트 메소드 추가

--- src/main/java/com/sporthustle/hustle/competitions/ingame/entity/PreRoundGroup.java
+++ src/main/java/com/sporthustle/hustle/competitions/ingame/entity/PreRoundGroup.java
@@ -5,6 +5,7 @@ import com.sporthustle.hustle.competitions.competition.entity.Competition;
 import com.sporthustle.hustle.competitions.entryteam.entity.EntryTeam;
 import javax.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -28,4 +29,17 @@ public class PreRoundGroup extends BaseEntity {
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "entry_team_id", nullable = false)
   private EntryTeam entryTeam;
+
+  @Builder
+  private PreRoundGroup(String name) {
+    this.name = name;
+  }
+
+  public void updateCompetition(Competition competition) {
+    this.competition = competition;
+  }
+
+  public void updateEntryTeam(EntryTeam entryTeam) {
+    this.entryTeam = entryTeam;
+  }
 }
