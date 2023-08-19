947a4a82e4d106a2d3b6247573c1608e4cbf4f4f
Min Gyu <kimmingyu9811@gmail.com>
2023-08-19 21:00:49 +0900

    fix: 구체적인 출처 추가

--- src/main/java/com/sporthustle/hustle/common/config/WebConfig.java
+++ src/main/java/com/sporthustle/hustle/common/config/WebConfig.java
@@ -17,7 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**") // 프로그램에서 제공하는 URL
-        .allowedOrigins("*") // 청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
+        .allowedOrigins(
+            "http://localhost:8080",
+            "http://localhost:3000") // 청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
         .allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
         .allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST...)
         .allowCredentials(
