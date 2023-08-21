d93f2354f52243e513456e5076d3cadbfb021a2c
kor-Chipmunk <rhj4862@gmail.com>
2023-08-22 01:04:16 +0900

    feat: 스프링 시큐리티에 CORS 도메인 허용 로직 추가
    * CorsConfigurationSource 빈 등록하여 CORS 설정
    * `/api/user/**` 경로를 `"USER"` 역할만 갈 수 있는 설정 중복되어 삭제

--- src/main/java/com/sporthustle/hustle/common/config/security/SecurityConfig.java
+++ src/main/java/com/sporthustle/hustle/common/config/security/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.sporthustle.hustle.common.config.security;
 
 import com.sporthustle.hustle.common.config.security.filter.JwtAuthenticationFilter;
+import com.sporthustle.hustle.common.consts.Constants;
 import com.sporthustle.hustle.common.jwt.JwtTokenProvider;
+import com.sporthustle.hustle.user.entity.UserRole;
+import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +16,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -33,39 +40,48 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable();
-    http.httpBasic()
+    http.csrf()
         .disable()
+        .httpBasic()
+        .disable()
+        .cors()
+        .configurationSource(corsConfigurationSource())
+        .and()
         .authorizeRequests()
-        .antMatchers("/", "/api/auth/**", "/api/university", "/api/user/find/**")
+        .antMatchers(getPermitAllPatterns().toArray(new String[0]))
         .permitAll()
-        .antMatchers("/v2/api-docs")
-        .permitAll()
-        .antMatchers("/configuration/ui")
-        .permitAll()
-        .antMatchers("/swagger-resources/**")
-        .permitAll()
-        .antMatchers("/configuration/security")
-        .permitAll()
-        .antMatchers("/swagger-ui.html")
-        .permitAll()
-        .antMatchers("/swagger-ui/*")
-        .permitAll()
-        .antMatchers("/webjars/**")
-        .permitAll()
-        .antMatchers("/v2/**")
-        .permitAll()
-        .antMatchers("/api/**")
+        .antMatchers(getAuthenticatedPatterns().toArray(new String[0]))
         .authenticated()
-        .antMatchers("/api/admin/**")
-        .hasRole("ADMIN")
-        .antMatchers("/api/user/**")
-        .hasRole("USER")
+        .antMatchers(getAdminRolePatterns().toArray(new String[0]))
+        .hasRole(UserRole.ADMIN.name())
         .and()
         .addFilterBefore(
             new JwtAuthenticationFilter(jwtTokenProvider),
-            UsernamePasswordAuthenticationFilter.class)
-        .cors();
+            UsernamePasswordAuthenticationFilter.class);
     http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  }
+
+  private List<String> getPermitAllPatterns() {
+    return List.of("/", "/api/auth/**", "/api/university", "/api/user/find/**", "/swagger-ui/**");
+  }
+
+  private List<String> getAuthenticatedPatterns() {
+    return List.of("/api/**");
+  }
+
+  private List<String> getAdminRolePatterns() {
+    return List.of("/api/admin/**");
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Constants.ALLOWED_DOMAIN_ORIGINS);
+    configuration.setAllowedMethods(Arrays.asList(Constants.PERMIT_ALL));
+    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
