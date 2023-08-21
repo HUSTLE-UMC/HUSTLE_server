e8fd8e031239bb0a9a56e8f407c443ad94db3de7
kor-Chipmunk <rhj4862@gmail.com>
2023-08-22 05:49:32 +0900

    feat: `MatchResultPostCategory` `of` 메서드 추가 & `InGameCategory` 와 호환되도록 `from` 메서드 작성

--- src/main/java/com/sporthustle/hustle/competitions/match/entity/MatchResultPostCategory.java
+++ src/main/java/com/sporthustle/hustle/competitions/match/entity/MatchResultPostCategory.java
@@ -1,6 +1,37 @@
 package com.sporthustle.hustle.competitions.match.entity;
 
+import com.sporthustle.hustle.competitions.ingame.dto.InGameCategory;
+
 public enum MatchResultPostCategory {
   PREROUND,
   FINAL,
+  ;
+
+  public static MatchResultPostCategory of(String matchResultPostCategoryString) {
+    if (matchResultPostCategoryString == null) {
+      throw new IllegalArgumentException("matchResultPostCategory 이 null 입니다.");
+    }
+
+    for (MatchResultPostCategory matchResultPostCategory : MatchResultPostCategory.values()) {
+      if (matchResultPostCategory.name().equals(matchResultPostCategoryString)) {
+        return matchResultPostCategory;
+      }
+    }
+
+    return MatchResultPostCategory.PREROUND;
+  }
+
+  public static MatchResultPostCategory from(InGameCategory inGameCategory) {
+    if (inGameCategory == null) {
+      throw new IllegalArgumentException("inGameCategory 이 null 입니다.");
+    }
+
+    for (MatchResultPostCategory matchResultPostCategory : MatchResultPostCategory.values()) {
+      if (matchResultPostCategory.name().equals(inGameCategory.name())) {
+        return matchResultPostCategory;
+      }
+    }
+
+    return MatchResultPostCategory.PREROUND;
+  }
 }
