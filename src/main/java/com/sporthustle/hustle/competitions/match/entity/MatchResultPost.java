5fffa72162f9b07988cce6bd40c00daef5302469
kor-Chipmunk <rhj4862@gmail.com>
2023-08-21 19:11:35 +0900

    feat: `MatchResultPost` 엔티티에 홈팀, 어웨이팀 관련 경기 분석 정보 추가
    * `MatchResultPostClub` 엔티티를 없앤 대신 `MatchResultPost` 엔티티에 반정규화 진행

--- src/main/java/com/sporthustle/hustle/competitions/match/entity/MatchResultPost.java
+++ src/main/java/com/sporthustle/hustle/competitions/match/entity/MatchResultPost.java
@@ -2,6 +2,8 @@ package com.sporthustle.hustle.competitions.match.entity;
 
 import com.sporthustle.hustle.common.entity.BaseEntity;
 import com.sporthustle.hustle.competitions.competition.entity.Competition;
+import com.sporthustle.hustle.competitions.entryteam.entity.EntryTeam;
+import java.time.LocalDateTime;
 import javax.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -28,11 +30,37 @@ public class MatchResultPost extends BaseEntity {
       name = "group_category",
       nullable = false,
       length = 10,
-      columnDefinition = "CHAR(10) default ''")
+      columnDefinition = "VARCHAR(20) default ''")
   private String groupCategory;
 
   @Column(name = "post_order", nullable = false, columnDefinition = "SMALLINT UNSIGNED default 1")
   private Integer postOrder;
+
+  @Column(name = "match_time", nullable = false)
+  private LocalDateTime matchTime;
+
+  @Column(name = "media_url", nullable = false, length = 60)
+  private String mediaUrl;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "home_entry_team_id", nullable = false)
+  private EntryTeam homeEntryTeam;
+
+  @Column(name = "home_score", nullable = false, columnDefinition = "SMALLINT UNSIGNED default 0")
+  private Integer homeScore;
+
+  @Column(name = "is_home_win", columnDefinition = "TINYINT(1) default 0")
+  private boolean isHomeWin = false;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "away_entry_team_id", nullable = false)
+  private EntryTeam awayEntryTeam;
+
+  @Column(name = "away_score", nullable = false, columnDefinition = "SMALLINT UNSIGNED default 0")
+  private Integer awayScore;
+
+  @Column(name = "is_away_win", columnDefinition = "TINYINT(1) default 0")
+  private boolean isAwayWin = false;
 
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "competition_id", nullable = false)
