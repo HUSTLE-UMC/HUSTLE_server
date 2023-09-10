24d26e222bc3be23dfc7a5b2fd5611f97bf09969
kor-Chipmunk <rhj4862@gmail.com>
2023-09-10 22:16:41 +0900

    refactor: `HttpURLConnection` & `JsonParser` 로직을 `WebClient` 로직으로 변경

--- src/main/java/com/sporthustle/hustle/oauth/OAuthService.java
+++ src/main/java/com/sporthustle/hustle/oauth/OAuthService.java
@@ -2,21 +2,23 @@ package com.sporthustle.hustle.oauth;
 
 import static com.sporthustle.hustle.common.consts.Constants.*;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import com.sporthustle.hustle.common.exception.BaseException;
-import com.sporthustle.hustle.common.jwt.dto.KakaoTokenInfo;
-import com.sporthustle.hustle.oauth.dto.OAuthUserInfoResponseDTO;
-import com.sporthustle.hustle.oauth.utils.OAuthConnector;
-import com.sporthustle.hustle.oauth.utils.kakao.KakaoInformationParser;
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import com.sporthustle.hustle.common.exception.ErrorCode;
+import com.sporthustle.hustle.common.jwt.dto.TokenInfo;
+import com.sporthustle.hustle.oauth.dto.GetKakaoTokenResponseDTO;
+import com.sporthustle.hustle.oauth.dto.GetKakaoUserInformationResponseDTO;
+import com.sporthustle.hustle.oauth.dto.KakaoAPIGetUserResponseDTO;
+import com.sporthustle.hustle.oauth.dto.KakaoAPITokenResponseDTO;
+import com.sporthustle.hustle.oauth.exception.BadGatewayException;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.util.retry.Retry;
 
 @Slf4j
 @Service
@@ -32,74 +34,85 @@ public class OAuthService {
   @Value("${oauth.kakao.secret.redirect-url}")
   private String redirectUrl;
 
-  public KakaoTokenInfo getKakaoToken(String code) {
-    String access_Token = "";
-    String refresh_Token = "";
-    String reqURL = KAKAO_REQUEST_URL_FOR_TOKEN;
+  private final WebClient webClient;
 
-    try {
-      URL url = new URL(reqURL);
-      HttpURLConnection conn = connector.getConnectionForKakaoToken(url);
+  public GetKakaoTokenResponseDTO getKakaoToken(String code) {
+    KakaoAPITokenResponseDTO response =
+        webClient
+            .post()
+            .uri(KAKAO_REQUEST_URL_FOR_TOKEN)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(
+                BodyInserters.fromFormData("grant_type", "authorization_code")
+                    .with("client_id", clientId)
+                    .with("redirect_uri", redirectUrl)
+                    .with("code", code))
+            .retrieve()
+            .onStatus(
+                httpStatus -> httpStatus.is4xxClientError(),
+                clientResponse ->
+                    clientResponse
+                        .bodyToMono(String.class)
+                        .map(message -> BaseException.from(ErrorCode.INVALID_AUTH_CODE)))
+            .onStatus(
+                httpStatus -> httpStatus.is5xxServerError(),
+                clientResponse ->
+                    clientResponse
+                        .bodyToMono(String.class)
+                        .map(message -> new BadGatewayException()))
+            .bodyToMono(KakaoAPITokenResponseDTO.class)
+            .timeout(Duration.ofSeconds(3L))
+            .retryWhen(
+                Retry.backoff(3, Duration.ofSeconds(3L))
+                    .filter(throwable -> throwable instanceof BadGatewayException))
+            .block();
 
-      // POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
-      connector.transportKakaoUrl(conn, clientId, redirectUrl, code);
-
-      // 결과 코드가 200이라면 성공
-      printLogForConnection(conn);
-
-      // 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-      String jsonResultForToken = kakaoInformationParser.getJsonForKakaoToken(conn);
-
-      // Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
-      JsonParser parser = new JsonParser();
-      JsonElement element = parser.parse(jsonResultForToken);
-
-      access_Token = kakaoInformationParser.getAccessToken(element);
-      refresh_Token = kakaoInformationParser.getRefreshToken(element);
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    return KakaoTokenInfo.builder().accessToken(access_Token).refreshToken(refresh_Token).build();
+    return GetKakaoTokenResponseDTO.builder()
+        .code("SUCCESS_GET_KAKAO_TOKEN")
+        .message("성공적으로 토큰을 조회했습니다.")
+        .data(
+            TokenInfo.builder()
+                .accessToken(response.accessToken)
+                .refreshToken(response.refreshToken)
+                .build())
+        .build();
   }
 
-  public OAuthUserInfoResponseDTO getKakaoUserInfo(String token) throws BaseException {
+  public GetKakaoUserInformationResponseDTO getKakaoUserInformation(String kakaoAccessToken)
+      throws BaseException {
+    KakaoAPIGetUserResponseDTO response =
+        webClient
+            .post()
+            .uri(KAKAO_REQUEST_URL_FOR_INFORMATION)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .header("Authorization", "Bearer " + kakaoAccessToken)
+            .retrieve()
+            .onStatus(
+                httpStatus -> httpStatus.is4xxClientError(),
+                clientResponse ->
+                    clientResponse
+                        .bodyToMono(String.class)
+                        .map(message -> BaseException.from(ErrorCode.INVALID_AUTH_CODE)))
+            .onStatus(
+                httpStatus -> httpStatus.is5xxServerError(),
+                clientResponse ->
+                    clientResponse
+                        .bodyToMono(String.class)
+                        .map(message -> new BadGatewayException()))
+            .bodyToMono(KakaoAPIGetUserResponseDTO.class)
+            .timeout(Duration.ofSeconds(3L))
+            .retryWhen(
+                Retry.backoff(3, Duration.ofSeconds(3L))
+                    .filter(throwable -> throwable instanceof BadGatewayException))
+            .block();
 
-    String reqURL = KAKAO_REQUEST_URL_FOR_INFORMATION;
-
-    // access_token을 이용하여 사용자 정보 조회
-    try {
-      URL url = new URL(reqURL);
-      HttpURLConnection conn = connector.getConnectionForUserInformation(url, token);
-
-      // 결과 코드가 200이라면 성공
-      printLogForConnection(conn);
-
-      // 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
-      String jsonResult = kakaoInformationParser.getJsonForKakaoUserInformation(conn);
-
-      // Gson 라이브러리로 JSON파싱
-      JsonParser parser = new JsonParser();
-      JsonElement element = parser.parse(jsonResult);
-
-      String snsId = kakaoInformationParser.getSnsIdByParsing(element);
-      String email = kakaoInformationParser.getEmailByParsing(element);
-      String name = kakaoInformationParser.getNameByParsing(element);
-      String gender = kakaoInformationParser.getGenderByParsing(element);
-
-      return OAuthUserInfoResponseDTO.builder()
-          .oauthId(snsId)
-          .email(email)
-          .name(name)
-          .password(getRamdomPassword())
-          .gender(gender)
-          .build();
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return null;
+    return GetKakaoUserInformationResponseDTO.builder()
+        .oauthId(response.snsId)
+        .email(response.getKakaoAccount().getEmail())
+        .name(response.getKakaoAccount().getProfile().getNickname())
+        .password(getRandomPassword())
+        .gender(response.getKakaoAccount().getGender())
+        .build();
   }
 
   private String getRamdomPassword() {
