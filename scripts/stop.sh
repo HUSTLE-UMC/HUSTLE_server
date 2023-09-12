9cda2846ff56b3181941a35cff840357c9c1d32f
kor-Chipmunk <rhj4862@gmail.com>
2023-09-12 23:36:21 +0900

    fix: 디스코드 삭제 & 정지 스크립트 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
 HOME_ROOT="/home/ubuntu"
-PROJECT_ROOT="/home/ubuntu/deploy"
-JAR_DIRECTORY="$PROJECT_ROOT/build/libs"
 
-cd $JAR_DIRECTORY
-
-LATEST_JAR=$(ls -t | grep -v "plain" | grep jar -m 1)
-JAR="$JAR_DIRECTORY/$LATEST_JAR"
-
-APP_LOG="$HOME_ROOT/logs/application.log"
-ERROR_LOG="$HOME_ROOT/logs/error.log"
 STOP_LOG="$HOME_ROOT/logs/stop.log"
 
-SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
+SERVICE_PID=$(pgrep -f jar) # 실행중인 Spring 서버의 PID
 
 if [ -z "$SERVICE_PID" ]; then
   echo "Not Found" >> $STOP_LOG
